3b3c94251bce2bbdbf5cc490748f338647f347e6
FOCC <mihocc18@gmail.com>
2024-01-06 00:32:51 +0300

    Обновляет стили

---
+++ docs/source/_static/js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready( function() {
+	$('section > h1 + ul, \
+		section > h2 + ul, \
+		section > h3 + ul, \
+		section > h4 + ul, \
+		section > h5 + ul, \
+		section > h6 + ul').addClass('traits');
+
+    // Добавляет иконкам действий заголовок всплывающий при наведение.
+    // Невозможно реализовать в Sphinx, так как изображения не имеют нужного атрибута.
+    $("img.action, img.game-icon").each(function() {
+    	$( this ).attr("title", $( this ).attr("alt"));
+    });
+});
