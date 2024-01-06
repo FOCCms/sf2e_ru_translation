b635673918998b6583dee26950f535d563f0f58e
FOCC <mihocc18@gmail.com>
2024-01-06 23:51:09 +0300

    Добавляет описание признака область

--- docs/source/_static/js/traits_tooltips_v1.23.js
+++ docs/source/_static/js/traits_tooltips_v1.23.js
@@ -30,7 +30,8 @@ $(document).ready( function() {
 	// Остальные признаки действий, существ, предметов и т.п.
 
 	traits_dict = {
-		
+		"attack": "Атака (attack): Умение с этим признаком подразумевают атаку. В ваш ход, для каждой атаки после первой, вы получаете штраф множественной атаки.",
+		"area": "Область (area): Это оружие может стрелять только с использованием действия \"Огонь по области (Area Fire)\"",
 		// "11111111": "22222222 (11111111): 33333333",
 	}
 
@@ -48,6 +49,10 @@ $(document).ready( function() {
 
 	// $('ul.traits li:textEquals("22222222")').tooltipster({content: traits_dict["11111111"]});
 
+	$('ul.traits li:textEquals("атака")').tooltipster({content: traits_dict["attack"]});
+	$('ul.traits li:textEquals("область")').tooltipster({content: traits_dict["area"]});
+
 
 	// $('span.t-11111111').tooltipster({content: traits_dict["11111111"]});
+
 });
